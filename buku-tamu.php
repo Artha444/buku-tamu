3c4992371133018822722cbed050d16c42bfc5cd
= <=>
2025-09-14 23:46:15 +0700

    menambahkan fitur upload gambar di buku_tamu

--- buku-tamu.php
+++ buku-tamu.php
@@ -1,162 +1,161 @@
-<?php
-require_once('function.php');
-include_once('templates/header.php');
-
-if (($_SESSION['role']) != 'operator') {
-  echo "<script>alert('Anda tidak memiliki akses')</script>";
-  echo "<script>window.location.href='index.php'</script>";
-}
-?>
-
-<!--a Begin Page Content -->
-<div class="container-fluid">
-
-  <!-- Page Heading -->
-  <h1 class="h3 mb-4 text-gray-800">Buku Tamu</h1>
-
-  <?php
-  if (isset($_POST['simpan'])) {
-    if (tambah_tamu($_POST) > 0) {
+ <?php
+  require_once('function.php');
+  include_once('templates/header.php');
   ?>
-      <div class="alert alert-success">
-        Data berhasil disimpan!
-      </div>
-    <?php
-    } else {
+
+ <!-- Begin Page Content -->
+ <div class="container-fluid">
+
+   <!-- Page Heading -->
+   <h1 class="h3 mb-4 text-gray-800">Buku Tamu</h1>
+
+   <?php
+    // Jika ada tombol simpan
+    if (isset($_POST['simpan'])) {
+      if (tambah_tamu($_POST) > 0) {
     ?>
-      <div class="alert alert-danger">
-        Data gagal disimpan!
-      </div>
-  <?php
+       <div class="alert alert-success" role="alert">
+         Data berhasil disimpan!
+       </div>
+   <?php
+      }
     }
-  }
+    ?>
+
+   <!-- DataTales Example -->
+   <div class="card shadow mb-4">
+     <div class="card-header py-3">
+       <button type="button" class="btn btn-primary btn-icon-split"
+         data-toggle="modal" data-target="#tambahModal">
+         <span class="icon text-white-50">
+           <i class="fas fa-plus"></i>
+         </span>
+         <span class="text">Data Tamu</span>
+       </button>
+     </div>
+     <div class="card-body">
+       <div class="table-responsive">
+         <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+           <thead>
+             <tr>
+               <th>No</th>
+               <th>Tanggal</th>
+               <th>Nama Tamu</th>
+               <th>Alamat</th>
+               <th>No. Telp/HP</th>
+               <th>Bertemu Dengan</th>
+               <th>Kepentingan</th>
+               <th>Aksi</th>
+             </tr>
+           </thead>
+           <tbody>
+             <?php
+              $no = 1;
+
+              $buku_tamu = query("SELECT * FROM buku_tamu");
+              foreach ($buku_tamu as $tamu) : ?>
+               <tr>
+                 <td><?= $no++; ?></td>
+                 <td><?= $tamu['tanggal'] ?></td>
+                 <td><?= $tamu['nama_tamu'] ?></td>
+                 <td><?= $tamu['alamat'] ?></td>
+                 <td><?= $tamu['no_hp'] ?></td>
+                 <td><?= $tamu['bertemu'] ?></td>
+                 <td><?= $tamu['kepentingan'] ?></td>
+                 <td>
+                   <a class="btn btn-success" href="edit-tamu.php?id=<?= $tamu['id_tamu'] ?>">Ubah</a>
+                   <a onclick="confirm('Apakah anda yakin ingin menghapus data ini?')" class="btn btn-danger"
+                     href="hapus-tamu.php?id=<?= $tamu['id_tamu'] ?>">Hapus</a>
+                 </td>
+               </tr>
+             <?php endforeach; ?>
+           </tbody>
+         </table>
+       </div>
+     </div>
+   </div>
+
+ </div>
+ <!-- /.container-fluid -->
+
+ <?php
+  // mangambil data barang dari tabel dengan kode tarbesar
+  $query = mysqli_query($koneksi, "SELECT max(id_tamu) as kodeTerbesar From buku_tamu");
+  $data = mysqli_fetch_array($query);
+  $kodeTamu = $data['kodeTerbesar'];
+
+  // mengambil angka dari kode barang terbesar
+  $urutan = (int) substr($kodeTamu, 2, 3);
+
+  //nomor yang diambil akan bertambah 1 dan menentukan urutannya
+  $urutan++;
+
+  //membuat kode barang baru
+  $huruf = "xt";
+  $kodeTamu = $huruf . sprintf("%03s", $urutan);
   ?>
 
-  <!-- DataTales Example -->
-  <div class="card shadow mb-4">
-    <div class="card-header py-3">
-      <button type="button" class="btn btn-primary btn-icon-split" data-toggle="modal" data-target="#tambahModal">
-        <span class="icon text-white-50">
-          <i class="fas fa-plus"></i>
-        </span>
-        <span class="text">Data Tamu</span>
-      </button>
-    </div>
-    <div class="card-body">
-      <div class="table-responsive">
-        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-          <thead>
-            <tr>
-              <th>No</th>
-              <th>Tanggal</th>
-              <th>Nama Tamu</th>
-              <th>Alamat</th>
-              <th>No. Telp/HP</th>
-              <th>Bertemu dg.</th>
-              <th>Kepentingan</th>
-              <th>Aksi</th>
-            </tr>
-          </thead>
-          <tbody>
-            <?php
-            $no = 1;
+ <!-- Modal Tambah-->
+ <div class="modal fade" id="tambahModal" tabindex="-1" aria-labelledby="tambahModalLabel" aria-hidden="true">
+   <div class="modal-dialog">
+     <div class="modal-content">
+       <div class="modal-header">
+         <h5 class="modal-title" id="tambahModalLabel">Modal title</h5>
+         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+           <span aria-hidden="true">&times;</span>
+         </button>
+       </div>
+       <div class="modal-body">
+         <form method="post" action="" enctype="multipart/form-data">
+           <input type="hidden" name="id_tamu" id="id_tamu" value="<?= $kodeTamu ?>">
+           <div class="form-group row">
+             <label for="nama_tamu" class="col-sm-3 col-form-label">Nama Tamu</label>
+             <div class="col-sm-8">
+               <input type="text" class="form-control" name="nama_tamu" id="nama_tamu">
+             </div>
+           </div>
+           <div class="form-group row">
+             <label for="alamat" class="col-sm-3 col-form-label">Alamat</label>
+             <div class="col-sm-8">
+               <textarea class="form-control" name="alamat" id="alamat"></textarea>
+             </div>
+           </div>
+           <div class="form-group row">
+             <label for="no_hp" class="col-sm-3 col-form-label">No. Telepon</label>
+             <div class="col-sm-8">
+               <input type="text" class="form-control" name="no_hp" id="no_hp">
+             </div>
+           </div>
+           <div class="form-group row">
+             <label for="bertemu" class="col-sm-3 col-form-label">Bertemu dg</label>
+             <div class="col-sm-8">
+               <input type="text" class="form-control" name="bertemu" id="bertemu">
+             </div>
+           </div>
+           <div class="form-group row">
+             <label for="kepentingan" class="col-sm-3 col-form-label">Kepentingan</label>
+             <div class="col-sm-8">
+               <input type="text" class="form-control" name="kepentingan" id="kepentingan">
+             </div>
+           </div>
+           <div class="form-group row">
+             <label for="gambar" class="col-sm-3 col-form-label">Unggah Foto</label>
+             <div class="col-sm-8">
+               <input type="file" class="custom-file-input" name="gambar" id="gambar">
+               <label for="gambar" class="custom-file-label">Choose file</label>
+             </div>
+           </div>
+           <div class="modal-footer">
+             <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+             <button type="submit" name="simpan" class="btn btn-primary">Simpan</button>
+           </div>
+         </form>
+       </div>
+     </div>
+   </div>
+ </div>
 
-            $buku_tamu = query("SELECT * FROM buku_tamu");
-            foreach ($buku_tamu as $tamu) : ?>
-              <tr>
-                <td><?= $no++; ?></td>
-                <td><?= $tamu['tanggal'] ?></td>
-                <td><?= $tamu['nama_tamu'] ?></td>
-                <td><?= $tamu['alamat'] ?></td>
-                <td><?= $tamu['no_hp'] ?></td>
-                <td><?= $tamu['bertemu'] ?></td>
-                <td><?= $tamu['kepentingan'] ?></td>
-                <td>
-                  <a class="btn btn-success" href="edit-tamu.php?id=<?= $tamu['id_tamu'] ?>">Ubah</a>
-                  <a onclick="confirm('apakah anda yakin ingin menghaous data ini?')" class="btn btn-danger" href="hapus-tamu.php?id=<?= $tamu['id_tamu'] ?>">Hapus</a>
-                </td>
-              </tr>
-            <?php endforeach; ?>
-          </tbody>
-        </table>
-      </div>
-    </div>
-  </div>
-</div>
 
-</div>
-<!-- /.container-fluid -->
-<?php
-// mangambil data barang dari tabel dengan kode tarbesar
-$query = mysqli_query($koneksi, "SELECT max(id_tamu) as kodeTerbesar From buku_tamu");
-$data = mysqli_fetch_array($query);
-$kodeTamu = $data['kodeTerbesar'];
-
-// mengambil angka dari kode barang terbesar
-$urutan = (int) substr($kodeTamu, 2, 3);
-
-//nomor yang diambil akan bertambah 1 dan menentukan urutannya
-$urutan++;
-
-//membuat kode barang baru
-$huruf = "zt";
-$kodeTamu = $huruf . sprintf("%03s", $urutan);
-?>
-
-<!-- Modal -->
-<div class="modal fade" id="tambahModal" tabindex="-1" aria-labelledby="tambahModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="tambahModalLabel">Tambah Data Tamu</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <div class="modal-body">
-          <form method="post" action="">
-            <input type="hidden" name="id_tamu" id="id_tamu" value="<?= $kodeTamu ?>">
-            <div class="form-group row">
-              <label for="nama_tamu" class="col-sm-3 col-form-label">Nama Tamu</label>
-              <div class="col-sm-8">
-                <input type="text" class="form-control" id="nama_tamu" name="nama_tamu">
-              </div>
-            </div>
-            <div class="form-group row">
-              <label for="alamat" class="col-sm-3 col-form-label">Alamat</label>
-              <div class="col-sm-8">
-                <textarea class="form-control" id="alamat" name="alamat"></textarea>
-              </div>
-            </div>
-            <div class="form-group row">
-              <label for="no_hp" class="col-sm-3 col-form-label">No. Telepon</label>
-              <div class="col-sm-8">
-                <input type="text" class="form-control" id="no_hp" name="no_hp">
-              </div>
-            </div>
-            <div class="form-group row">
-              <label for="bertemu" class="col-sm-3 col-form-label">Bertemu dg. </label>
-              <div class="col-sm-8">
-                <input type="text" class="form-control" id="bertemu" name="bertemu">
-              </div>
-            </div>
-            <div class="form-group row">
-              <label for="kepentingan" class="col-sm-3 col-form-label">Kepentingan</label>
-              <div class="col-sm-8">
-                <input type="text" class="form-control" id="kepentingan" name="kepentingan">
-              </div>
-            </div>
-        </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Keluar</button>
-        <button type="submit" name="simpan" class="btn btn-primary">Simpan</button>
-      </div>
-    </div>
-  </div>
-  </form>
-
-  <?php
+ <?php
   include_once('templates/footer.php');
   ?>
